f9583d2e4d2fe3f288433d78dfdf6b6b648c54c4
Rodrigo Pereira <pereirarodrigo420@gmail.com>
2024-12-06 10:33:26 +0000

    Fix: Online Search Only

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ async def detailed_research(user_input):
     detailed_report = DetailedReport(
         query=user_input,
         report_type="research_report",
-        report_source="hybrid",
     )
 
     report = await detailed_report.run()
@@ -40,7 +39,6 @@ async def research(user_input):
     researcher = GPTResearcher(
         query=user_input,
         report_type="detailed_report",
-        report_source="hybrid",
     )
 
     await researcher.conduct_research()
